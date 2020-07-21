b38a9d147b478e4c373dd8d1f6fc607b11e226ea
d3vkk <donaldkk22@yahoo.com>
2020-07-21 15:15:04 +0300

    feat: Click button to scrape new data
    Data being word and syllables

--- src/components/WordGrab.vue
+++ src/components/WordGrab.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="p-8 m-4 text-center">
-    <div ref="gottenWord" class="text-4xl font-bold"></div>
-    <div ref="gottenSyllables" class="text-2xl italic"></div>
+  <div>
+    <div class="p-8 m-4 text-center">
+      <div ref="gottenWord" class="text-4xl font-bold"></div>
+      <div ref="gottenSyllables" class="text-2xl italic"></div>
+    </div>
+    <div class="p-8 m-4 text-center">
+      <button
+        class="py-2 px-6 m-4 shadow text-2xl border-2 border-black"
+        title="get word"
+        ref="getWord"
+        @click="getWord()"
+      >Hunt</button>
+    </div>
   </div>
 </template>
 
