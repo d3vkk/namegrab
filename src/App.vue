6ead627b1fe9e9169f5ae7298442180082c651b2
d3vkk <donaldkk22@yahoo.com>
2020-07-21 14:32:25 +0300

    feat: Scrape data from NLP website
    Data being word and syllables

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
+    <WordGrab/>
   </div>
 </template>
 
 <script>
+import WordGrab from './components/WordGrab.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    WordGrab
+  }
 }
 </script>
 
---
+++ src/components/WordGrab.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="p-8 m-4 text-center">
+    <div id="gotten-word" class="text-4xl font-bold"></div>
+    <div id="gotten-syllables" class="text-2xl italic"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    async getWord() {
+      const res = await fetch(`http://localhost:3000/word`);
+      if (res.status != 200) {
+        console.log(res.status);
+      } else {
+        const gottenWord = JSON.parse(await res.json());
+        document.querySelector("#gotten-word").innerHTML = gottenWord.word;
+        document.querySelector("#gotten-syllables").innerHTML =
+          gottenWord.syllables;
+      }
+    }
+  },
+  created() {
+    this.getWord();
+  }
+};
+</script>
+
+<style scoped>
+</style>
