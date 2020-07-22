70173995352f48fdb280bfee5d37141bc1e7c036
d3vkk <donaldkk22@yahoo.com>
2020-07-22 09:42:35 +0300

    fix: Allow frontend url to access backend response

--- server/app.js
+++ server/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:2000');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.redirect('https://wordgrab.netlify.app');
 })
