45d9af8c11248fdd539f87bf781fdfb78ec121ee
d3vkk <donaldkk22@yahoo.com>
2020-07-23 17:05:50 +0300

    v0.3.0

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const fetch = require("node-fetch");
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:2000');
+  res.header('Access-Control-Allow-Origin', 'https://namegrab.netlify.app/name');
   next();
 });
 
